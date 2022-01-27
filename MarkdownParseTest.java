bf2e0f0fd35afd935fc10671838b9a7a4f799807
Daphne-wu <71472654+Daphne-wu@users.noreply.github.com>
2022-01-27 14:12:23 -0800

    Update MarkdownParseTest.java
    test 6 had an error, output page.com when it was supose to be empty

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -47,12 +47,32 @@ public class MarkdownParseTest {
 
     @Test
     public void parseTest4() throws IOException {
-        Path fileName = Path.of("test-file3.md");
+        Path fileName = Path.of("test-file4.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         System.out.println(links);
         ArrayList<String> expected = new ArrayList<>();
         expected.add("https://something.com");
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void parseTest5() throws IOException {
+        Path fileName = Path.of("test-file5.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        System.out.println(links);
+        ArrayList<String> expected = new ArrayList<>();
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void parseTest6() throws IOException {
+        Path fileName = Path.of("test-file6.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        System.out.println(links);
+        ArrayList<String> expected = new ArrayList<>();
         assertEquals(expected, links);
     }
 
