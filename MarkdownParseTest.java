247c6bf61a808291a136ce8ce8329c2c66ea5f9c
Daphne-wu <71472654+Daphne-wu@users.noreply.github.com>
2022-02-03 13:27:09 -0800

    test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -73,7 +73,7 @@ public class MarkdownParseTest {
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         System.out.println(links);
         ArrayList<String> expected = new ArrayList<>();
-        expected.add("test.com")
+        expected.add("test.com");
         assertEquals(expected, links);
     }
 
